cebd9445d0c70c8b1c278b252d08f1eb4c3f5dbe
pnelson1337 <pnelson1337@gmail.com>
2018-10-09 14:48:46 -0400

    Changes all look good.

---
+++ SQL_QUERIES/OBS_ORDER.sql
@@ -0,0 +1,10 @@
+SELECT
+ROW_NUMBER() OVER (PARTITION BY ORD.VisitID ORDER BY ORD.OrderID DESC)						AS OrderSeq
+,ORD.OrderedProcedureName,ORD.Status,PROV.Name,CONVERT(VARCHAR(10),ORD.OrderDateTime,101)	AS 'Order Date'
+,ORD.VisitID,ORD.OrderedProcedureMnemonic,AV.AccountNumber,AV.Name,BV.AdmitDateTime					
+FROM			[Livedb].[dbo].[OeOrders]													AS ORD
+LEFT JOIN		[Livedb].[dbo].[BarVisits]													AS BV										ON ORD.VisitID=BV.VisitID
+LEFT JOIN		[Livedb].[dbo].[AdmVisits]													AS AV										ON ORD.VisitID=AV.VisitID
+INNER JOIN		[Livedb].[dbo].[DMisProvider]												AS PROV										ON ORD.ProviderID=PROV.ProviderID
+WHERE ORD.Status != 'CANCEL' AND ORD.OrderedProcedureMnemonic IN ('OBSERVICU','OBSERVMS','OBSERVOB') 
+AND BV.DischargeDateTime IS NULL AND BV.AdmitDateTime IS NULL AND AV.Status='ADM IN'
